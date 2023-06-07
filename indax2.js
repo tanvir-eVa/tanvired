5072488f68d0ff7b337c62f95990336fe27a3948
tanvir-eVa <125306854+tanvir-eVa@users.noreply.github.com>
2023-06-07 14:56:08 +0600

    Add files via upload

---
+++ indax2.js
@@ -0,0 +1,10 @@
+const DragArea = document.querySelector(".appBody"),
+DragText = DragArea.querySelector("h3"),
+button = DragArea.querySelector("button"),
+input = DragArea.querySelector("input");
+
+let Myfile ;
+
+  button.onclick = () => {
+    input.click();
+  }
